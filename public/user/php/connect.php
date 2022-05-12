651dc4efc36097254ed2792647aa975341f57ca4
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-05-12 14:30:41 +0700

    ...

--- public/user/php/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-	global $conn;
-	$conn=mysqli_connect('localhost','root','','minmovie');
-	//$conn=mysqli_connect('fdb22.awardspace.net','3173347_minmovies','3173347_minmovies','3173347_minmovies');
-	mysqli_set_charset($conn,'utf8');
- ?>
